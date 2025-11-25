69d6da5161271430c2da73ad7404056ac34fe6be
Fredy228 <smagrovich58@gmail.com>
2025-11-25 16:02:33 +0200

    Fix small bugs

--- client/app/(admin)/admin/page.tsx
+++ client/app/(admin)/admin/page.tsx
@@ -6,9 +6,6 @@ export default function DashboardPage() {
   return (
     <div style={{ padding: 20 }}>
       <Typography variant="h4">Добро пожаловать в Админку</Typography>
-      <Typography variant="body1">
-        Refine + MUI успешно интегрированы в ваш Next.js проект.
-      </Typography>
     </div>
   );
 }
