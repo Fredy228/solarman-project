8dc66a944307fc7fc966fcaa6d45125a2b3d1bb8
Fredy228 <smagrovich58@gmail.com>
2026-03-19 22:06:14 +0200

    Fix bugs

--- client/src/app/[locale]/(admin)/admin/page.tsx
+++ client/src/app/[locale]/(admin)/admin/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { Typography } from "@mui/material";
+import { useTranslations } from "next-intl";
 
 import ProtectProvider from "@/src/providers/protect-provider";
 
 export default function DashboardPage() {
+  const t = useTranslations("refine.dashboard");
+
   return (
     <ProtectProvider keyProvider="dashboard">
       <div style={{ padding: 20 }}>
-        <Typography variant="h4">Добро пожаловать в Админку</Typography>
+        <Typography variant="h4">{t("title")}</Typography>
       </div>
     </ProtectProvider>
   );
