4b70af17cc7be0cd95787507e0c6baabaa7f4662
Fredy228 <smagrovich58@gmail.com>
2026-03-20 16:54:30 +0200

    Fix load global param

--- server/src/api/global-param/global-param.module.ts
+++ server/src/api/global-param/global-param.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
+import { Module, type MiddlewareConsumer } from '@nestjs/common';
 
+import { ProtectAuthMiddleware } from 'src/common/middleware/auth/protect-auth.middleware';
 import { GlobalParamController } from './global-param.controller';
 import { GlobalParamService } from './global-param.service';
 
@@ -7,4 +8,8 @@ import { GlobalParamService } from './global-param.service';
   providers: [GlobalParamService],
   controllers: [GlobalParamController],
 })
-export class GlobalParamModule {}
+export class GlobalParamModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ProtectAuthMiddleware).forRoutes(GlobalParamController);
+  }
+}
