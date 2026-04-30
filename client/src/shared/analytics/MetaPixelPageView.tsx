2cd7b9ac98a0cff4491bf3ea3164328fdbd7ae86
Fredy228 <smagrovich58@gmail.com>
2026-04-30 12:27:51 +0300

    Fix bugs

--- client/src/shared/analytics/MetaPixelPageView.tsx
+++ client/src/shared/analytics/MetaPixelPageView.tsx
@@ -1,16 +1,15 @@
 "use client";
 
 import { trackMetaPixelPageView } from "@/src/libs/meta-pixel";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 export default function MetaPixelPageView() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     trackMetaPixelPageView();
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return null;
 }
